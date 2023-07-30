811dfac7603436863696347c53dba015190cfe3e
Neo <lordneobarnachea@gmail.com>
2023-07-30 15:34:13 +0800

    working with state

--- acs/src/components/Records.jsx
+++ acs/src/components/Records.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { TextField, Box, Button } from '@mui/material'
 import '../styles/record.css'
+import { useState } from 'react'
 //TODO: add students, see current students, convert attendance to xsml/ arrange sit plan
 export default function Records() {
+  const [firstname, setFirstName] = useState();
+  const [lastname, setLastName] = useState();
   return (
     <>
       <div className='record-container'>
@@ -13,13 +16,13 @@ export default function Records() {
           <h2>Add students</h2>
 
           <form className='addStudentForm'>
-            <Box sx={{display: 'flex', gap: '3em'}}>
-              <TextField label="First name" />
-              <TextField label="Last name" />
+            <Box sx={{ display: 'flex', gap: '3em' }}>
+              <TextField label="First name" onChange={(e) => setFirstName(e.target.value)} />
+              <TextField label="Last name" onChange={(e) => setLastName(e.target.value)} />
             </Box>
-            <Box sx={{display: 'flex', gap: '3em'}}>
-              <TextField label="Section"/>
-              <TextField label="Subject"/>
+            <Box sx={{ display: 'flex', gap: '3em' }}>
+              <TextField label="Section" />
+              <TextField label="Subject" />
             </Box>
             <Button>Submit</Button>
           </form>
